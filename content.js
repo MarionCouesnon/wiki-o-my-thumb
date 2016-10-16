54cc6d8d7d7dd295eb319cc4d81adf0bdeebc030
Mehdi Lahmam <mehdi@lahmam.com>
2016-10-16 21:33:10 +0200

    Save each link clicked when the game has started

--- content.js
+++ content.js
@@ -1,6 +1,36 @@
-console.log("Extension Wiki o' My Thumb loaded");
+console.log("Wiki o' My Thumb (WOMT) extension loaded");
 
-$("a").click(function() {
-  var clickedLink = $(this).attr("href");
-  chrome.runtime.sendMessage({"message": "generate_pdf", "url": clickedLink});
-});
+var currentURL = $(location).attr('href');
+var startingPoint = 'https://fr.m.wikipedia.org/wiki/Cacaoyer';
+
+if (currentURL == startingPoint) {
+  // opening ceremony of the game
+  console.log("WOMT game started");
+  localStorage.setItem("womt_current_game", "[]")
+}
+
+if (game = currentGame()) {
+  storeClickedLinkFor(game);
+}
+
+function currentGame() {
+  return JSON.parse(localStorage.getItem("womt_current_game"));
+}
+
+function storeClickedLinkFor(currentGame) {
+  // track clicked links using jQuery
+  $("a").click(function() {
+
+    // create an object with the link href and title
+    var link = {
+      href: $(this).attr("href"),
+      text: $(this).text()
+    }
+
+    // add this object to the links bucket
+    currentGame.push(link);
+
+    // store this bucket back
+    localStorage.setItem("womt_current_game", JSON.stringify(currentGame));
+  });
+}
