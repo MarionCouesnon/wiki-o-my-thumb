43a985ae3eb0f179333d6f998fb305914af5e77a
Mehdi Lahmam <mehdi@lahmam.com>
2016-10-16 21:33:28 +0200

    End the game by generating a PDF with links and destroying localStorage data
    It sends a message to the background script that does the PDF generation.
    We need to send the current_game object as background script can't access
    localStorage shared with content scripts.

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "generate_pdf" ) {
-      generatePDF(request.url)
+    if( request.message === "womt_game_ended" ) {
+      generateResultsFor(request.current_game);
     }
   }
 );
 
-
-function generatePDF(url) {
+function generateResultsFor(game) {
   var pdf = new jsPDF();
 
-  pdf.text(url);
-  pdf.save("wiki-o-my-thumb.pdf");
+  for (var i = 0; i < game.length; i++) {
+    pdf.text(20, 20 + (i * 10), game[i].text + " " + "(" + game[i].href + ")");
+  }
+
+  pdf.save("womt.pdf");
 }
--- content.js
+++ content.js
@@ -2,11 +2,23 @@ console.log("Wiki o' My Thumb (WOMT) extension loaded");
 
 var currentURL = $(location).attr('href');
 var startingPoint = 'https://fr.m.wikipedia.org/wiki/Cacaoyer';
+var endingPoint = 'https://fr.m.wikipedia.org/wiki/Unterseeboot';
 
 if (currentURL == startingPoint) {
   // opening ceremony of the game
   console.log("WOMT game started");
   localStorage.setItem("womt_current_game", "[]")
+}
+
+else if (currentURL == endingPoint) {
+  // closing ceremony
+  console.log("WOMT game ended");
+  chrome.runtime.sendMessage({ "message": "womt_game_ended", "current_game": currentGame() });
+
+  // remove all the data stored locally
+  // to keep only one "womt_current_game" variable stored.
+  // This helps to detect if a game is in progress or not in content.js script.
+  localStorage.removeItem("womt_current_game")
 }
 
 if (game = currentGame()) {
