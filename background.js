010c7f0642c71dfdfee8bfe2046e741d9b3e017e
kevinvennitti <kevin.vennitti@gmail.com>
2017-06-03 13:12:32 +0200

    Add factor bonus styles

--- background.js
+++ background.js
@@ -9,7 +9,25 @@ chrome.runtime.onMessage.addListener(
 function generateResultsFor(game) {
   console.log('womt_game_ended');
   
+  var factor = game.links.length;
+  var dotAttributes = '';
+  
+  if (factor < 3) {
+    dotAttributes += 'edge[color=blue]';
+  }
+  if (factor >= 3 && < 6) {
+    dotAttributes += 'edge[color=red]';
+  }
+  if (factor >= 6 && < 9) {
+    dotAttributes += 'edge[color=yellow]';
+  }
+  if (factor >= 9) {
+    dotAttributes += 'edge[color=green]';
+  }
+  
   var dot = 'digraph D { ';
+  
+  dot += dotAttributes;
   
   dot += escapeWordForDot(game.startPage);
   dot += ' -> ';
